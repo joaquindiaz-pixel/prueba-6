2843a8098256951711bc35e03096bf242bc8dbeb
joaquindiaz-pixel <joaquin.diaz@uc.cl>
2026-04-15 12:50:15 -0400

    Delete shared directory

--- shared/js/core.js
+++
@@ -1,4 +0,0 @@
-gsap.to(".progress-bar",{width:"100%",scrollTrigger:{trigger:"body",scrub:1}});
-function animateCounter(el,end){
-gsap.fromTo(el,{innerText:0},{innerText:end,duration:2,snap:{innerText:1}});
-}
